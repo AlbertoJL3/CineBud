178751c8342280066fca63413e872b8bf541ed5e
AlbertoJL3 <363alberto@gmail.com>
2024-07-15 10:30:38 -0400

    MongoDB cluster created and connected

--- ml_backend/data_collection.py
+++ ml_backend/data_collection.py
@@ -1,6 +1,7 @@
 # Script to collect movie data from TMDb API
 import requests
-import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -8,13 +9,22 @@ from pymongo import MongoClient
 # Load environment variables from the .env file
 load_dotenv()
 
+# Mongo Atlas URI
+uri = os.environ.get('MONGO_URI')
 # Set up the TMDb API key
 api_key = os.getenv('OMDB_API_KEY')
 
-#Connect to MongoDB
-client  = MongoClient('mongodb://localhost:27017/')
-db = client['movie_recommendation']
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client['moviesdb']
 collection = db['movies']
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 def fetch_and_store_movie_data(movie_title):
     url = f'http://www.omdbapi.com/?t={movie_title}&apikey={api_key}&plot=full'
@@ -30,7 +40,11 @@ def fetch_and_store_movie_data(movie_title):
                 'actors': data['Actors'].split(', '),
                 'director': data['Director'].split(', '),
                 'plot': data['Plot'],
-                'release_date': data['Released']
+                'release_date': data['Released'], 
+                'rotten_tomatoes': data['Ratings'][1]['Value'] if len(data['Ratings']) > 1 else None,
+                'poster': data['Poster'],
+                'runtime': data['Runtime'],
+                'imdb_rating': data['imdbRating'],
             }
             collection.insert_one(movie_data)
             print(f'Inserted {movie_title} to DB')
@@ -38,3 +52,8 @@ def fetch_and_store_movie_data(movie_title):
             print(f"Movie not found: {movie_title}")
     else:
         print(f"Failed to fetch data for: {movie_title}")
+
+# List of movies to fetch data for
+movies = ['The Shawshank Redemption', 'The Godfather', 'Pulp Fiction'] 
+for movie in movies: 
+    fetch_and_store_movie_data(movie)
