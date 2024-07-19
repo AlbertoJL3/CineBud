9c382970a615eee7578d6f0738d02bd47f5035d5
Beto Lopez <363alberto@gmail.com>
2024-07-19 12:29:25 -0400

    Added ChatGPT functionality-version controlled to 0.28

--- backend/models/gpt.py
+++ backend/models/gpt.py
@@ -1,25 +1,22 @@
 import openai
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
 
 # Load environment variables from the .env file
 load_dotenv()
 
-client = OpenAI(
-  api_key = os.getenv('OPEN_API_KEY')  # this is also the default, it can be omitted
-)
+# Set the OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')  # Ensure this matches the key name in your .env file
 
 def get_chatgpt_response(input_prompt):
-    response = client.completions.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        prompt = input_prompt
+        messages=[
+            {"role": "system", "content": "You are a movie recommender. Recommend 40 movies at a time. Only recommend movie title and year in json format."},
+            {"role": "user", "content": input_prompt}
+        ]
     )
-    return response['choices'][0]['message']['content'].strip()
+    print(response['usage'])
+    return response['choices'][0]['message']['content']
 
-
-
-# Example usage
-prompt = "Can you recommend a feel-good comedy movie?"
-response = get_chatgpt_response(prompt)
-print(response)
+print(get_chatgpt_response('I feel like a happy ending.'))
